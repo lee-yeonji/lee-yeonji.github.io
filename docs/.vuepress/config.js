78b271ae4e4aa9663bd97d36440b1d178df5051a
lee-yeonji <duswl4314@gmail.com>
2022-03-31 22:24:19 +0900

    message

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -55,7 +55,7 @@ module.exports = {
         children: ['/javascript/js']
       },
       {
-        title: 'react',
+        title: 'react~',
         collapsable: true,
         children: ['/react/jsx']
       }
