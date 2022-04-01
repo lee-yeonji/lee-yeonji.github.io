61da3daab27a30751b30cf11166b1421137d4b6a
lee-yeonji <duswl4314@gmail.com>
2022-04-01 21:01:45 +0900

    message

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,9 +17,18 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/config/#head
    */
   head: [
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
+    ["link", { rel: "shortcut icon", href: "/favicon.ico" }],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "192x192",
+        href: "images/maskable_icon_x192.png",
+      },
+    ],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }], 
+    [ 'meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }, ],
   ],
 
   /**
@@ -67,7 +76,20 @@ module.exports = {
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
   plugins: [
+    [
     '@vuepress/plugin-back-to-top',
     '@vuepress/plugin-medium-zoom',
+    ],
+  
+    [
+    "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "새 컨텐츠가 등록되었습니다. 새로고침 버튼을 눌러주세요😀",
+          buttonText: "새로고침",
+        },
+      },
+    ],
   ]
 }
