cf493b8402792a9b5d5adf167321ccc604bcc34e
lee-yeonji <duswl4314@gmail.com>
2022-04-01 21:30:38 +0900

    message

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -28,17 +28,8 @@ module.exports = {
       },
     ],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }], 
-    [ 'meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }, ],
-    ["link", { rel: "shortcut icon", href: "./favicon.ico" }], 
-    ["link", { rel: "manifest", href: "/manifest.json" }],
-    [
-      "link",
-      {
-        rel: "apple-touch-icon",
-        sizes: "192x192",
-        href: "images/maskable_icon_x192.png",
-      },
-    ],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }, ],
+    
   ],
 
   /**
